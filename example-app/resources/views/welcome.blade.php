561b1dd4b14a57b0fbf1eabde2baf60499a7e334
4ITCLR <142764666+4ITCLR@users.noreply.github.com>
2023-10-18 20:11:14 +0800

    Add files via upload

---
+++ example-app/resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<header>
+    <h1>Welcome</h1>
+</header>
+
+<nav>
+    <ul>
+        <li><a href='about'>about</a></li>
+       
+    </ul>
+</nav>
+
+
+
+</body>
+</html>
